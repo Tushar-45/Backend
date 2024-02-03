50328ad215b2ece2b0b62fcc37074d7a7dfe95f7
Tushar <tpanchal219>
2024-02-03 11:16:27 +0530

    adding models

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ connectDB()
     app.listen(process.env.PORT || 8000,()=>{
         console.log(`server running at port: ${process.env.PORT}`)
     })
-
 })
 .catch((err)=>{
     console.log("Mongo db connection failed !!! ", err)
---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,
+        requried: true,
+    },
+    thumbnail: {
+        type : String,
+        requried: true
+    },
+    title: {
+        type : String,
+        requried: true
+    },
+    description: {
+        type : String,
+        requried: true
+    },
+    duration: {
+        type : Number,
+        requried: true
+    },
+    views:{
+        type: Number,
+        default: 0
+    },
+    isPublished:{
+        type: Boolean,
+        default: true,
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref : "User"
+    }
+
+},{
+    timestamps:true
+}) 
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export default Video = mongoose.model("Video", videoSchema);
